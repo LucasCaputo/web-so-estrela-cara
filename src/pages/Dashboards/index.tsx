56a84e0597f0b92a5a902b5c43bb36c5fda755af
Caputo <lccaputo@hotmail.com>
2020-11-11 00:31:59 -0300

    feat: adicionar layout da home

--- src/pages/Dashboards/index.tsx
+++ src/pages/Dashboards/index.tsx
@@ -1,13 +1,84 @@
 import React from "react";
+
 import LinkerPageHeader from "../../components/LinkerPageHeader";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
 import "./styles.css";
 
 function Dashboards() {
   return (
     <div id="Dashboards">
-      <div className="dashboards-content">
-        <LinkerPageHeader name="Senhor Barriga" to="/home"></LinkerPageHeader>
+      <LinkerPageHeader name="Senhor Barriga" to="/home"></LinkerPageHeader>
+
+      <div className="dashboards-container">
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Paper className="primary-cards">
+              <VisibilityOffIcon></VisibilityOffIcon>
+              <h4>
+                Saldo <br />
+                Disponível
+              </h4>
+              <h6>
+                <span>R$</span> 10.000,00
+              </h6>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className="primary-cards">
+              <h4>Gastos</h4>
+            </Paper>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Paper className="secondary-cards">
+              <VisibilityOffIcon></VisibilityOffIcon>
+              <h4>Gastos</h4>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className="secondary-cards">
+              <VisibilityOffIcon></VisibilityOffIcon>
+              <h4>Gastos</h4>
+            </Paper>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Paper className="secondary-cards">
+              <VisibilityOffIcon></VisibilityOffIcon>
+              <h4>Gastos</h4>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className="secondary-cards">
+              <VisibilityOffIcon></VisibilityOffIcon>
+              <h4>Gastos</h4>
+            </Paper>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Paper className="secondary-cards">
+              <VisibilityOffIcon></VisibilityOffIcon>
+              <h4>Gastos</h4>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className="secondary-cards">
+              <VisibilityOffIcon></VisibilityOffIcon>
+              <h4>Gastos</h4>
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
