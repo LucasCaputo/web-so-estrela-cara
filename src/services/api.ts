75aad0748c56f5731d89a8c03ee8acb7a5f4bf4b
Caputo <lccaputo@hotmail.com>
2020-11-15 15:27:19 -0300

    fix: corrige url api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://api.mega-hack.devmatheus.com/",
+  baseURL: "https://api.mega-hack.devmatheus.com/",
   // baseURL: "http://localhost:3333/",
 });
 
